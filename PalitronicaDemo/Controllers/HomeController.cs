45a406b77637fec1f3c43efced2b0ddc0a504785
Bhargav <bhargavpatel2022@gmail.com>
2023-07-14 20:31:20 -0500

    Docker Implemented

--- PalitronicaDemo/Controllers/HomeController.cs
+++ PalitronicaDemo/Controllers/HomeController.cs
@@ -2,7 +2,9 @@
 using Microsoft.EntityFrameworkCore;
 using PalitronicaDemo.Model;
 using PalitronicaDemo.ViewModels;
+using System.Configuration;
 using System.Text.Json;
+using Taxjar;
 
 namespace PalitronicaDemo.Controllers
 {
@@ -29,7 +31,7 @@ namespace PalitronicaDemo.Controllers
         }
 
         [HttpGet("/Home/GetCustomers")]
-        public async Task<ICollection<Customer>> GetCustomers()
+        public async Task<ICollection<Model.Customer>> GetCustomers()
         {
             var Customers = await _context.Customers.ToListAsync();
 
@@ -43,57 +45,76 @@ namespace PalitronicaDemo.Controllers
 
             return items;
         }
+        [HttpPost]
+        [Route(("/Home/GetItemsRate"))]
+        public async Task<Item> GetItemsRate([FromBody] int Id)
+        {
+            var items = await _context.Items.Where(x => x.ItemId == Id.ToString()).SingleOrDefaultAsync();
+
+            return items;
+        }
+        //[HttpPost]
+        //public async Task<IActionResult> CalculatePayment(Customer request)
+        //{
+        //    var customer = await _context.Customers.FindAsync(request.CustomerId);
+        //    if (customer == null)
+        //        return BadRequest("Customer not found.");
+
+        //    // Calculate total price and taxes
+        //    decimal totalPrice = 0.0m;
+        //    decimal totalTaxes = 0.0m;
+        //    Dictionary<string, decimal> itemPrices = new Dictionary<string, decimal>();
+
+        //    foreach (var item in request.Items)
+        //    {
+        //        // Retrieve item details from the external API based on the item ID
+        //        var itemDetails = await GetCustomerTaxRate(customer.zip);
+        //        if (itemDetails == null)
+        //            return BadRequest($"Item with ID {item.ItemId} not found.");
+
+        //        decimal itemPrice = item.Price * item.Quantity;
+        //        decimal itemTaxes = itemPrice * Convert.ToDecimal(itemDetails.combined_rate);
+
+        //        itemPrices.Add(item.ItemId, itemPrice);
+        //        totalPrice += itemPrice;
+        //        totalTaxes += itemTaxes;
+        //    }
+
+        //    decimal totalPriceWithTaxes = totalPrice + totalTaxes;
+
+        //    // Create the payment response
+        //    var customerResponse = new CustomerVM
+        //    {
+        //        CustomerName = customer.CustomerName,
+        //        // TotalItemsPrice = itemPrices,
+        //        TotalTaxes = totalTaxes,
+        //        TotalPrice = totalPriceWithTaxes
+        //    };
+
+        //    return Ok(customerResponse);
+        //}
 
         [HttpPost]
-        public async Task<IActionResult> CalculatePayment(Customer request)
-        {
-            var customer = await _context.Customers.FindAsync(request.CustomerId);
-            if (customer == null)
-                return BadRequest("Customer not found.");
-
-            // Calculate total price and taxes
-            decimal totalPrice = 0.0m;
-            decimal totalTaxes = 0.0m;
-            Dictionary<string, decimal> itemPrices = new Dictionary<string, decimal>();
-
-            foreach (var item in request.Items)
-            {
-                // Retrieve item details from the external API based on the item ID
-                var itemDetails = await GetItemTaxes(customer.zip);
-                if (itemDetails == null)
-                    return BadRequest($"Item with ID {item.ItemId} not found.");
-
-                decimal itemPrice = item.Price * item.Quantity;
-                decimal itemTaxes = itemPrice * Convert.ToDecimal(itemDetails.combined_rate);
-
-                itemPrices.Add(item.ItemId, itemPrice);
-                totalPrice += itemPrice;
-                totalTaxes += itemTaxes;
-            }
-
-            decimal totalPriceWithTaxes = totalPrice + totalTaxes;
-
-            // Create the payment response
-            var customerResponse = new CustomerVM
-            {
-                CustomerName = customer.CustomerName,
-                // TotalItemsPrice = itemPrices,
-                TotalTaxes = totalTaxes,
-                TotalPrice = totalPriceWithTaxes
-            };
-
-            return Ok(customerResponse);
-        }
-
-        private async Task<ApiResponse> GetItemTaxes(string Zip)
+        [Route("/Home/GetCustomerTaxRate")]
+        public async Task<ApiResponse> GetCustomerTaxRate([FromBody] int CustomerId)
         {
             // Make an API request to retrieve item details based on the item ID
             // Replace the API endpoint and logic with your actual implementation
             // You can use HttpClient or any other HTTP client library
+            var customerZip = await _context.Customers.Where(a => a.CustomerId == CustomerId.ToString()).SingleOrDefaultAsync();
 
             var httpClient = _httpClientFactory.CreateClient();
-            var apiUrl = $"https://api.example.com/items/{Zip}";
+            var apiUrl = $"https://api.taxjar.com/v2/rates/:{customerZip.zip}";
 
+            var client = new TaxjarApi("64ace41a0344d6ae9a4c4220a0c7dac1");
+
+            var tax =await client.RatesForLocationAsync(customerZip.zip, new
+            {
+                city = "LOS ANGELES",
+                country = "US"
+            });
+            
+         
             var response = await httpClient.GetAsync(apiUrl);
             if (response.IsSuccessStatusCode)
             {
--- PalitronicaDemo/ViewModels/CustomerVM.cs
+++ PalitronicaDemo/ViewModels/CustomerVM.cs
@@ -3,7 +3,7 @@
     public class CustomerVM
     {
         public string CustomerName { get; set; }
-        public decimal Items { get; set; }
+        public string Items { get; set; }
         public decimal TotalItemsPrice { get; set; }
         public decimal TotalTaxes { get; set; }
         public decimal TotalPrice { get; set; }
